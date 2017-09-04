55e5ffd0ef90518bc027b27360cc04b43954e371
adina-moldovan <adina_moldovan29@yahoo.com>
2017-09-04 23:47:49 +0300

    Start adding pagination for List

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -10,15 +10,19 @@ namespace SportsStore.WebUI.Controllers
     public class ProductController : Controller
     {
         private IProductsRepository productRepository;
+        public int PageSize = 4;
 
         public ProductController(IProductsRepository repository)
         {
             productRepository = repository;
         }
 
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(productRepository.Products);
+            return View(productRepository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((page -1) * PageSize)
+                .Take(PageSize));
         }
     }
 }
---
+++ SportsStore.WebUI/HtmlHelpers/PagingHelpers.cs
@@ -0,0 +1,35 @@
+﻿using SportsStore.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.HtmlHelpers
+{
+    public static class PagingHelpers
+    {
+        public static MvcHtmlString PageLinks(this HtmlHelper html, PagingInfo paginingInfo, 
+            Func<int, string> pageUrl)
+        {
+            var result = new StringBuilder();
+            for (int i = 1; i <= paginingInfo.TotalPages; i++)
+            {
+                var tag = new TagBuilder("a");
+                tag.MergeAttribute("href", pageUrl(i));
+                tag.InnerHtml = i.ToString();
+                if (i == paginingInfo.CurrentPage)
+                {
+                    tag.AddCssClass("selected");
+                    tag.AddCssClass("btn-primary");
+                }
+
+                tag.AddCssClass("btn btn-default");
+                result.Append(tag.ToString());
+
+            }
+            return MvcHtmlString.Create(result.ToString());
+        }
+    }
+}
