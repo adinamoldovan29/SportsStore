1704ff0bd684461d92026508cedd0bc3eac28650
adina-moldovan <adina_moldovan29@yahoo.com>
2017-09-04 23:56:02 +0300

    Add the test class

---
+++ SportsStore.UnitTests/ProductControllerTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
+using System.Collections.Generic;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
+using System.Linq;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class ProductControllerTest
+    {
+        [TestMethod]
+        public void List_Pagination_ReturnsPage()
+        {
+            // Arange
+            Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product>(){
+                new Product { ProductID = 3,  Name = "P3"},
+                new Product { ProductID = 2, Name = "P2"},
+                new Product { ProductID = 1, Name = "P1"}
+            });
+
+            var productController = new ProductController(mock.Object);
+            productController.PageSize = 2;
+
+            // Act
+            var secondPage = productController.List(2).Model as IEnumerable<Product>;
+
+            // Assert
+            var secondPageProd = secondPage.ToList();
+            Assert.AreEqual(1, secondPageProd.Count());
+            Assert.AreEqual("P3", secondPage.First().Name);
+        }
+    }
+}
