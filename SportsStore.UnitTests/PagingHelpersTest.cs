266ca85c1561f0a1277ae2ad54c54ce1e9c83668
Adina Moldovan <adina.moldovan@amdaris.com>
2017-09-05 17:07:31 +0300

    New test file

---
+++ SportsStore.UnitTests/PagingHelpersTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class PagingHelpersTest
+    {
+        [TestMethod]
+        public void PageLinks__ProduceCorectHtml()
+        {
+            System.Web.Mvc.HtmlHelper html = null;
+           
+        }
+    }
+}
