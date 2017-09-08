5e65bc7c20ee5b983f815effa68e90e4fb962501
Adina Moldovan <adina.moldovan29@gmail.com>
2017-09-08 09:14:49 +0300

    Add unit test for paging info

--- SportsStore.UnitTests/ProductControllerTest.cs
+++ SportsStore.UnitTests/ProductControllerTest.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using SportsStore.Domain.Entities;
 using SportsStore.WebUI.Controllers;
 using System.Linq;
+using SportsStore.WebUI.Models;
 
 namespace SportsStore.UnitTests
 {
@@ -13,26 +14,54 @@ namespace SportsStore.UnitTests
     public class ProductControllerTest
     {
         [TestMethod]
-        public void List_Pagination_ReturnsPage()
+        public void List_Pagination_ReturnsProductsPaginated()
         {
             // Arange
-            Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
+            var productsRepoMoack = CreateProductsRepoMock();
+
+            var productController = new ProductController(productsRepoMoack.Object);
+            productController.PageSize = 2;
+
+            // Act
+            var model = productController.List(2).Model as ProductsListViewModel;
+
+            // Assert
+            Assert.IsNotNull(model, "Model is null");
+            Assert.AreEqual(1, model.Products.Count());
+            Assert.AreEqual("P3", model.Products.First().Name);
+        }
+
+        [TestMethod]
+        public void List_Pagination_ReturnsPagingInfoCorrect()
+        {
+            // Arange
+            var productsRepoMoack = CreateProductsRepoMock();
+
+            var productController = new ProductController(productsRepoMoack.Object);
+            productController.PageSize = 2;
+
+            // Act
+            var model = productController.List(2).Model as ProductsListViewModel;
+
+            // Assert
+            Assert.IsNotNull(model, "Model is null");
+
+            Assert.AreEqual(2, model.PagingInfo.CurrentPage, "CurrentPage is not correct");
+            Assert.AreEqual(2, model.PagingInfo.TotalPages, "TotalPages is not correct");
+            Assert.AreEqual(3, model.PagingInfo.TotalItems, "TotalItem value is not correct");
+            Assert.AreEqual(2, model.PagingInfo.ItemsPerPage, "ItemPerPage value is not correct");
+        }
+
+        private Mock<IProductsRepository> CreateProductsRepoMock()
+        {
+            var mock = new Mock<IProductsRepository>();
             mock.Setup(m => m.Products).Returns(new List<Product>(){
                 new Product { ProductID = 3,  Name = "P3"},
                 new Product { ProductID = 2, Name = "P2"},
                 new Product { ProductID = 1, Name = "P1"}
             });
 
-            var productController = new ProductController(mock.Object);
-            productController.PageSize = 2;
-
-            // Act
-            var secondPage = productController.List(2).Model as IEnumerable<Product>;
-
-            // Assert
-            var secondPageProd = secondPage.ToList();
-            Assert.AreEqual(1, secondPageProd.Count());
-            Assert.AreEqual("P3", secondPage.First().Name);
+            return mock;
         }
     }
 }
