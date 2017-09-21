355ed0ec82ba84705f62bbd34c1e441ff75b65a0
Adina Moldovan <adina.moldovan29@gmail.com>
2017-09-21 18:27:58 +0300

    Partial implementation of categories navigation bar

---
+++ SportsStore.WebUI/Controllers/NavigationController.cs
@@ -0,0 +1,30 @@
+﻿using SportsStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class NavigationController : Controller
+    {
+        private IProductsRepository prodRepo;
+
+        public NavigationController(IProductsRepository prodRepository)
+        {
+            prodRepo = prodRepository;
+        }
+
+        // GET: Navigation
+        public PartialViewResult Menu()
+        {
+            var cat = prodRepo.Products
+                .Select(p => p.Category)
+                .Distinct()
+                .OrderBy(c => c);
+
+            return PartialView(cat);
+        }
+    }
+}
