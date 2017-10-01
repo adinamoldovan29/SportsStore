e8dec74d164a370e10dba20eaf4a5ece1e5a5d82
Adina Moldovan <adina.moldovan29@gmail.com>
2017-10-01 12:30:40 +0300

    Add cart

---
+++ SportsStore.Domain/Entities/Cart.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.Domain.Entities
+{
+    public class Cart
+    {
+        private List<CartLine> cartLineCollection = new List<CartLine>();
+
+        public IEnumerable<CartLine> CartLines
+        {
+            get { return cartLineCollection; }
+        }
+
+        public void AddToCart(Product product, int quantity)
+        {
+            var line = cartLineCollection
+                .Where(cl => cl.Product.ProductID == product.ProductID)
+                .SingleOrDefault();
+
+            if (line != null)
+            {
+                line.Quantity += quantity;
+            }
+            else
+            {
+                cartLineCollection.Add(new CartLine()
+                {
+                    Product = product,
+                    Quantity = quantity
+                });
+            }
+
+        }
+
+        public decimal ComputeCartValue()
+        {
+            return cartLineCollection.Sum(cl => cl.Product.Price * cl.Quantity);
+        }
+
+        public void RemoveLine(Product product)
+        {
+            cartLineCollection.RemoveAll(cl => cl.Product.ProductID == product.ProductID);
+        }
+
+        public void ClearCart()
+        {
+            cartLineCollection.Clear();
+        }
+    }
+
+    public class CartLine
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ SportsStore.UnitTests/CartTest.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SportsStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class CartTest
+    {
+        [TestMethod]
+        public void AddToCart_2Products_Added()
+        {
+            // Arange
+            var prod = new Product() { Name = "ProdName" };
+            var cart = new Cart();
+
+            // Act
+            cart.AddToCart(prod, 2);
+
+            // Assert
+            Assert.AreEqual(1, cart.CartLines.Count(), "The number of CartLines is not correct");
+            Assert.AreEqual(2, cart.CartLines.First().Quantity, "Product quantity is not correct");
+            Assert.AreEqual("ProdName", cart.CartLines.First().Product.Name, "Product name is not correct");
+        }
+
+        [TestMethod]
+        public void ClearCart_2Products_CartEmpty()
+        {
+            // Arange
+             var cart = AddProductsToCart();
+
+            // Act
+            cart.ClearCart();
+
+            // Assert
+            Assert.AreEqual(0, cart.CartLines.Count(), "The number of CartLines is not correct");
+        }
+
+        [TestMethod]
+        public void RemoveLine_2Products_CartEmpty()
+        {
+            // Arange
+            var cart = AddProductsToCart();
+
+            // Act
+            cart.RemoveLine(cart.CartLines.First().Product);
+
+            // Assert
+            Assert.AreEqual(0, cart.CartLines.Count(), "The number of CartLines is not correct");
+        }
+
+        private Cart AddProductsToCart()
+        {
+            var prod = new Product() { Name = "ProdName" };
+
+            var cart = new Cart();
+            cart.AddToCart(prod, 2);
+
+            return cart;
+        }
+    }
+}
