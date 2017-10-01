07938364526fd7e93e39b6d0aa05310d52935888
adina-moldovan <adina_moldovan29@yahoo.com>
2017-10-01 18:56:20 +0300

    Add cart functionality to UI

---
+++ SportsStore.WebUI/Controllers/CartController.cs
@@ -0,0 +1,58 @@
+﻿using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private IProductsRepository repository;
+        public CartController(IProductsRepository repository) { 
+            this.repository = repository;
+        }
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel(){
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
+            });
+        }
+  
+        public RedirectToRouteResult AddtoCart(int productId, string returnUrl) { 
+            var product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
+
+            if (product != null) {
+                GetCart().AddToCart(product, 1);
+             }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult RemoveFromCart(int productId, string returnUrl)
+        {
+            var product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
+
+            if (product != null)
+            {
+                GetCart().RemoveLine(product);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        private Cart GetCart() {
+            var cart = (Cart)Session["Cart"];
+
+            if (cart == null) {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+
+            return cart;
+        }
+    }
+}
