68f569d7046debae55005863ba42dcda05e46ebe
adina-moldovan <adina_moldovan29@yahoo.com>
2017-10-01 13:55:56 +0300

    Unit tests for Cart class

--- SportsStore.UnitTests/CartTest.cs
+++ SportsStore.UnitTests/CartTest.cs
@@ -15,23 +15,49 @@ namespace SportsStore.UnitTests
         public void AddToCart_2Products_Added()
         {
             // Arange
-            var prod = new Product() { Name = "ProdName" };
+            var prod1 = new Product() { ProductID = 1, Name = "ProdName1" };
+            var prod2 = new Product() { ProductID = 2, Name = "ProdName2" };
             var cart = new Cart();
 
             // Act
-            cart.AddToCart(prod, 2);
+            cart.AddToCart(prod1, 2);
+            cart.AddToCart(prod2, 4);
 
             // Assert
-            Assert.AreEqual(1, cart.CartLines.Count(), "The number of CartLines is not correct");
-            Assert.AreEqual(2, cart.CartLines.First().Quantity, "Product quantity is not correct");
-            Assert.AreEqual("ProdName", cart.CartLines.First().Product.Name, "Product name is not correct");
+            Assert.AreEqual(2, cart.CartLines.Count(), "The number of CartLines is not correct");
+            Assert.AreEqual(2, cart.CartLines.Where(_ => _.Product == prod1).Single().Quantity, "Product1 quantity is not correct");
+            Assert.AreEqual(4, cart.CartLines.Where(_ => _.Product == prod2).Single().Quantity, "Product2 quantity is not correct");
         }
 
         [TestMethod]
-        public void ClearCart_2Products_CartEmpty()
+        public void AddToCart_ExistingProducts_IncreaseQuantity()
+        {
+            var prod1 = new Product() { ProductID = 1, Name = "ProdName1" };
+            var prod2 = new Product() { ProductID = 2, Name = "ProdName2" };
+            var cart = new Cart();
+
+            // Act
+            cart.AddToCart(prod1, 2);
+            cart.AddToCart(prod2, 4);
+            cart.AddToCart(prod1, 3);
+            cart.AddToCart(prod2, 2);
+
+            // Assert
+            Assert.AreEqual(2, cart.CartLines.Count(), "The number of CartLines is not correct");
+            Assert.AreEqual(5, cart.CartLines.Where(_ => _.Product == prod1).Single().Quantity, "Product1 quantity is not correct");
+            Assert.AreEqual(6, cart.CartLines.Where(_ => _.Product == prod2).Single().Quantity, "Product2 quantity is not correct");
+        }
+
+        [TestMethod]
+        public void ClearCart_2Lines_CartEmpty()
         {
             // Arange
-             var cart = AddProductsToCart();
+            var prod1 = new Product() {ProductID = 1, Name = "ProdName1"};
+            var prod2 = new Product() {ProductID = 2, Name = "ProdName2"};
+
+            var cart = new Cart();
+            cart.AddToCart(prod1, 2);
+            cart.AddToCart(prod2, 5);
 
             // Act
             cart.ClearCart();
@@ -41,26 +67,43 @@ namespace SportsStore.UnitTests
         }
 
         [TestMethod]
-        public void RemoveLine_2Products_CartEmpty()
+        public void RemoveLine_ManyProducts_ProductRemoved()
         {
             // Arange
-            var cart = AddProductsToCart();
-
-            // Act
-            cart.RemoveLine(cart.CartLines.First().Product);
-
-            // Assert
-            Assert.AreEqual(0, cart.CartLines.Count(), "The number of CartLines is not correct");
-        }
-
-        private Cart AddProductsToCart()
-        {
-            var prod = new Product() { Name = "ProdName" };
+            var prod1 = new Product() { ProductID = 1, Name = "ProdName1" };
+            var prod2 = new Product() { ProductID = 2, Name = "ProdName2" };
+            var prod3 = new Product() { ProductID = 3, Name = "ProdName3" };
 
             var cart = new Cart();
-            cart.AddToCart(prod, 2);
+            cart.AddToCart(prod3, 1);
+            cart.AddToCart(prod1, 2);
+            cart.AddToCart(prod2, 5);
+            cart.AddToCart(prod3, 1);
 
-            return cart;
+            // Act
+            cart.RemoveLine(prod3);
+
+            // Assert
+            Assert.AreEqual(0, cart.CartLines.Where(pl => pl.Product == prod3).Count(), "Produc was not removed from cart");
+        }
+
+        [TestMethod]
+        public void ComputeCartValue_2Products_CorrectValue()
+        {
+            // Arange
+            var prod1 = new Product() { ProductID = 1, Name = "ProdName1", Price = 10 };
+            var prod2 = new Product() { ProductID = 2, Name = "ProdName2", Price = 15 };
+
+            var cart = new Cart();
+            cart.AddToCart(prod1, 2);
+            cart.AddToCart(prod2, 1);
+            cart.AddToCart(prod1, 2);
+
+            // Act
+            var cartValue = cart.ComputeCartValue();
+
+            // Assert
+            Assert.AreEqual(55, cartValue, "Cart value not calculated correct");
         }
     }
 }
