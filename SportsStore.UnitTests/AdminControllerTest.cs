7772b54d13378d184b74277593558e20ae7cdbb5
adina-moldovan <adina.moldovan29@gmail.com>
2018-04-02 23:27:58 +0300

    Add Admin page and edit method

---
+++ SportsStore.UnitTests/AdminControllerTest.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SportsStore.Domain.Entities;
+using SportsStore.UnitTests.Helpers;
+using SportsStore.WebUI.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.UnitTests
+{
+    [TestClass]
+    public class AdminControllerTest
+    {
+        [TestMethod]
+        public void Index_ManyProducts_ReturnsAll()
+        {
+            var productRepo = RepositoryHelper.CreateProductsRepoMock();
+            var adminController = new AdminController(productRepo.Object);
+
+            var result = (IEnumerable<Product>)adminController.Index().ViewData.Model;
+
+            Assert.AreEqual(3, result.Count(), "Not all the product were rwturned");
+        }
+
+        [TestMethod]
+        public void Index_ManyProducts_ReturnsCorrectProducts()
+        {
+            var productRepo = RepositoryHelper.CreateProductsRepoMock();
+            var adminController = new AdminController(productRepo.Object);
+
+            var result = ((IEnumerable<Product>)adminController.Index().ViewData.Model).ToArray();
+
+            Assert.AreEqual("P1", result[2].Name);
+            Assert.AreEqual("P2", result[1].Name);
+            Assert.AreEqual("P3", result[0].Name);
+        }
+
+        [TestMethod]
+        public void Edit_OneProductRequested_ProductReturnedToView()
+        {
+            var productRepo = RepositoryHelper.CreateProductsRepoMock();
+            var adminController = new AdminController(productRepo.Object);
+
+            var resultProduct = adminController.Edit(2).ViewData.Model as Product;
+
+            Assert.AreEqual("P2", resultProduct.Name);
+        }
+
+        [TestMethod]
+        public void Edit_InexistingProductRequested_NullReturnedToView()
+        {
+            var productRepo = RepositoryHelper.CreateProductsRepoMock();
+            var adminController = new AdminController(productRepo.Object);
+
+            var resultProduct = adminController.Edit(10).ViewData.Model as Product;
+
+            Assert.IsNull(resultProduct);
+        }
+    }
+}
---
+++ SportsStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,33 @@
+﻿using SportsStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductsRepository productRepository;
+        public AdminController(IProductsRepository repo)
+        {
+            productRepository = repo;
+        }
+        
+        [HttpGet]
+        public ViewResult Index()
+        {
+            return View(productRepository.Products);
+        }
+
+        [HttpGet]
+        public ViewResult Edit(int ProductID)
+        {
+            var selectedProd = productRepository.Products
+                .FirstOrDefault(p => p.ProductID == ProductID);
+
+            return View(selectedProd);
+        }
+    }
+}
